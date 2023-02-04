d03bcdd7a9e988ffb1bca1e777f6cb7b23c56bdd
adriiglezs <adriiglezs0497@gmail.com>
2023-02-03 19:18:31 -0600

    Corrección de errores 01

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,26 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+//corregi lo que tiene entre parentesis
+const $n = document.querySelector('[name = "name"]');
+const $b = document.querySelector('[name ="blog"]');
+const $l = document.querySelector('[name ="location"]');
 
-function displayUser(username) {
+async function displayUser(username) { //agregue async
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  const data = await response.json();//agregue esta linea
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  //modifique comillas
+  $n.textContent = `${data.name}`;
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`;//agregue el signop de pesos
 }
+
 
 displayUser('stolinski').catch(handleError);
